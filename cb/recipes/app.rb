19909c988a8a4aff0afb33d5be4f548b9ef0bc9d
mallikarjunveepuru <malliveepuru@gmail.com>
2016-12-13 17:04:46 +0530

    Update app.rb

--- cb/recipes/app.rb
+++ cb/recipes/app.rb
@@ -1,71 +1,55 @@
 #
-# Cookbook Name:: b2app
+# Cookbook Name:: java_app
 # Recipe:: default
 #
 # Copyright 2016, YOUR_COMPANY_NAME
 #
 # All rights reserved - Do Not Redistribute
 #
+dbip1 = data_bag_item('db_ip','db_instance_ip')
+$db = dbip1['ip']
 
-
-remote_file '/tmp/b2artifact.tgz' do
-  source 'https://s3-us-west-2.amazonaws.com/prakash26790/b2artifact.tgz'
-  owner 'ec2-user'
-  group 'ec2-user'
- # mode '0755'
-  action :create
+execute "update_apt_repo" do
+	command "apt-get update"
 end
 
-execute "extract files" do
-  command "mkdir -p /tmp/web-app; tar -zxvf /tmp/b2artifact.tgz -C /tmp/web-app"
-  user 'ec2-user'
+package 'git' do
+	action :install
 end
 
-execute "move files" do
-  command " cp -r /tmp/web-app /home/ec2-user/"
-  user 'ec2-user'
+package 'maven' do
+	action :install
 end
 
-remote_file "Copy beehively file" do 
-  path "/etc/cron.d/beehively" 
-  source "file:///home/ec2-user/web-app/scripts/beehively"
-  owner 'ec2-user'
-  group 'ec2-user'
- # mode 0755
+execute "pull_app_repo" do
+	command "cd /home/ubuntu ; git clone https://github.com/mallikarjunveepuru/spring.git"
+	action :run
 end
 
-remote_file "Copy metrics file" do 
-  path "/home/ec2-user/bin/metrics.sh" 
-  source "file:///home/ec2-user/web-app/scripts/metrics.sh"
-  owner 'ec2-user'
-  group 'ec2-user'
- # mode 0755
+#template "/home/ubuntu/spring/src/main/resources/environment/db.properties" do
+
+#  source "db.properties.erb"
+
+#  variables( :ip => dbip1['ip'])
+
+#end
+
+execute "change_the_ip_of_db" do
+	command "echo #$db > /tmp/ip.txt ; sed -i -e \"s/localhost/#$db/g\" /home/ubuntu/spring/src/main/resources/environment/db.properties"
+#	command "/bin/bash /tmp/db.sh"
+	action :run
 end
 
-remote_file "Copy clock file" do 
-  path "/etc/sysconfig/clock" 
-  source "file:///home/ec2-user/web-app/scripts/clock"
-  #owner 'ec2-user'
-  #group 'ec2-user'
-  #mode 0755
+execute "compile maven project" do
+	command "cd /home/ubuntu/spring/ ; mvn clean install -U"
+	action :run
 end
 
-execute "BeforeInstall" do
-  command "sh /home/ec2-user/web-app/scripts/before.sh"
-  user 'ec2-user'
+package 'tomcat7' do
+	action :install
 end
 
-execute "AfterInstall" do
-  command "sh /home/ec2-user/web-app/scripts/after.sh"
-  user 'ec2-user'
-end
-
-execute "ApplicationStart" do
-  command "sh /home/ec2-user/web-app/scripts/start.sh"
-  user 'ec2-user'
-end
-
-execute "ValidateService" do
-  command "sh /home/ec2-user/web-app/scripts/validate.sh"
-  user 'ec2-user'
+execute "move artifacts to tomcat" do
+	command "export JAVA_HOME=\/usr\/lib\/jvm\/java-8-oracle ; export CATALINA_HOME=\/usr\/share\/tomcat7 ; sed -i -e \"s/java-7-oracle/java-7-oracle **\/usr\/lib\/jvm\/java-8-oracle**/g\" /etc/init.d/tomcat7 ; sudo mv /home/ubuntu/spring/target/spring-example.war /var/lib/tomcat7/webapps/ ; /etc/init.d/tomcat7 restart"
+	action :run
 end
